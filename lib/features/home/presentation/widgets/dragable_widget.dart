de8acd4a5846cd18f0694e775c1a2e5f85e9e29c
Abu Anwar MD Abdullah <abuanwar072@gmail.com>
2024-03-07 22:18:01 +0600

    12 done

--- lib/features/home/presentation/widgets/dragable_widget.dart
+++ lib/features/home/presentation/widgets/dragable_widget.dart
@@ -19,11 +19,72 @@ class DragableWidget extends StatefulWidget {
   State<DragableWidget> createState() => _DragableWidgetState();
 }
 
-class _DragableWidgetState extends State<DragableWidget> {
+class _DragableWidgetState extends State<DragableWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController restoreController;
+
+  final _widgetKey = GlobalKey();
+  Offset startOffset = Offset.zero;
+  Offset panOffset = Offset.zero;
+
+  Size size = Size.zero;
+  double angle = 0;
+
+  void onPanStart(DragStartDetails details) {
+    if (!restoreController.isAnimating) {
+      setState(() {
+        startOffset = details.globalPosition;
+      });
+    }
+  }
+
+  void onPanUpdate(DragUpdateDetails details) {
+    if (!restoreController.isAnimating) {
+      setState(() {
+        panOffset = details.globalPosition - startOffset;
+      });
+    }
+  }
+
+  void onPanEnd(DragEndDetails details) {
+    if (restoreController.isAnimating) {
+      return;
+    }
+    restoreController.forward();
+  }
+
+  @override
+  void initState() {
+    restoreController =
+        AnimationController(vsync: this, duration: kThemeAnimationDuration);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    restoreController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final child = SizedBox(key: _widgetKey, child: widget.child);
+    if (!widget.isEnableDrag) return child;
     return GestureDetector(
-      child: widget.child,
+      onPanStart: onPanStart,
+      onPanUpdate: onPanUpdate,
+      onPanEnd: onPanEnd,
+      child: AnimatedBuilder(
+        animation: restoreController,
+        builder: (context, child) {
+          final value = 1 - restoreController.value;
+          return Transform.translate(
+            offset: panOffset * value,
+            child: child,
+          );
+        },
+        child: child,
+      ),
     );
   }
 }
